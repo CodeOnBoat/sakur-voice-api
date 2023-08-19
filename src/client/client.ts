f1a7ca3618cc0508c1d22de93ddbbbce7a9b928f
Gonzalo <huntymonster@gmail.com>
2023-08-19 15:40:33 +0200

    done

--- src/client/client.ts
+++ src/client/client.ts
@@ -5,18 +5,12 @@ const root = "https://sakurapi.se/retendo-voice/api";
 
 export const sendVoiceMessage = async (voice: Blob, data: {}) => {
   const formData = new FormData();
-  formData.append("functionsToCallCollection", JSON.stringify(data));
-  formData.append("file", voice,{
-    filename: 'voice.mp4',
-    contentType: 'audio/mp4', 
-  });
-  console.log(voice);
-  console.log(formData);
-  const config = {
+  formData.append("functionsToCallCollection", data);
+  formData.append("file", voice);
+  const res = await axios.post(`${root}/voice`, formData, {
     headers: {
-      'Content-Type': 'multipart/form-data'
-    }
-  };
-  const res = await axios.post(`${root}/voice`, formData, config);
+      "Content-Type": "multipart/form-data",
+    },
+  });
   return res.data;
 };
